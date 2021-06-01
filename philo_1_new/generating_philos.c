a3d6eec96200c81106b48a1e4e66e3d22cf8c9c6
AyoubHolmes <boulbaz147@gmail.com>
2021-06-01 12:07:26 +0100

    Start working on the philo1's functionalities: Philo_1 done v0.4 perfecto

--- philo_1_new/generating_philos.c
+++ philo_1_new/generating_philos.c
@@ -1,14 +1,13 @@
 #include "philosophers.h"
 
-t_philos	*get_philo(int id, long init, pthread_mutex_t *forks, t_philo_parse *parse)
+t_philos	*get_philo(int id, pthread_mutex_t *forks, t_philo_parse *parse)
 {
 	t_philos *ph;
 
 	ph = (t_philos *)malloc(sizeof(t_philos));
-	pthread_mutex_init(&ph->life, NULL);
 	ph->s = id;
 	ph->forks = forks;
-	ph->init = init;
+	ph->init = parse->init;
 	ph->nbr_of_meals = parse->nbr_of_meals;
 	ph->parse = parse;
 	return (ph);
@@ -29,6 +28,7 @@ void	philo_lifecycle(t_philos *s)
 {
 	struct timeval	tp;
 
+	pthread_mutex_lock(&s->life);
 	pthread_mutex_lock(&(s)->forks[(s)->s - 1]);
 	philo_printer("has taken a fork\n", s, 0);
 	pthread_mutex_lock(&(s)->forks[((s)->s) % s->parse->nbr_philos]);
@@ -37,6 +37,7 @@ void	philo_lifecycle(t_philos *s)
 	philo_printer("is eating\n", s, s->parse->time_to_eat);
 	pthread_mutex_unlock(&(s)->forks[(s)->s - 1]);
 	pthread_mutex_unlock(&(s)->forks[((s)->s) % s->parse->nbr_philos]); 
+	pthread_mutex_unlock(&s->life);
 	philo_printer("is sleeping\n", s, s->parse->time_to_sleep);
 	philo_printer("is thinking\n", s, 0);
 }
@@ -46,14 +47,15 @@ void	*ft_death_philo(void *s)
 	t_philos *p;
 
 	p = (t_philos *)s;
-	while (1)
+	while (p->parse->alive)
 	{
+		usleep(100);
 		pthread_mutex_lock(&p->life);
 		if (ft_timer(0) > p->time_counter)
 		{
 			pthread_mutex_lock(p->parse->msg);
 			if (p->parse->alive == 1)
-			{	
+			{
 				if (p->nbr_of_meals == -2)
 					printf("%ld\t%d died\n",ft_timer(p->init), p->s);
 				else
@@ -61,10 +63,8 @@ void	*ft_death_philo(void *s)
 				p->parse->alive = 0;
 			}
 			pthread_mutex_unlock(p->parse->msg);
-			return (NULL);
 		}
 		pthread_mutex_unlock(&p->life);
-		usleep(100);
 	}
 	return (NULL);
 }
@@ -77,6 +77,7 @@ void	*ft_philosopher(void *s)
 
 	p = (t_philos *)s;
 	p->time_counter = ft_timer(0) + (long) p->parse->time_to_die;
+	pthread_mutex_init(&p->life, NULL);
 	pthread_create(&soul_reaper, NULL, ft_death_philo, p);
 	if (p->nbr_of_meals == -2)
 	{
@@ -93,9 +94,10 @@ int	ft_controller(t_philo_parse *philos)
 {
 	pthread_mutex_t		*forks;
 	t_philos			ph;
-	pthread_t			thread_id;
+	pthread_t			*thread_ids;
 	int					i;
 
+	thread_ids = (pthread_t *)malloc(sizeof(pthread_t));
 	forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * philos->nbr_philos);
 	i = 0;
 	while (i < philos->nbr_philos)
@@ -110,15 +112,15 @@ int	ft_controller(t_philo_parse *philos)
 	i = 0;
 	while (i < philos->nbr_philos)
 	{
-		pthread_create(&thread_id, NULL,
-			ft_philosopher, get_philo(i + 1, philos->init, forks, philos));
+		pthread_create(&thread_ids[i], NULL,
+			ft_philosopher, get_philo(i + 1, forks, philos));
 		usleep(100);
 		i++;
 	}
 	i = 0;
 	while (i < philos->nbr_philos)
 	{
-		pthread_join(thread_id, NULL);
+		pthread_join(thread_ids[i], NULL);
 		i++;
 	}
 	i = 0;
@@ -127,5 +129,7 @@ int	ft_controller(t_philo_parse *philos)
 		pthread_mutex_destroy(&forks[i]);
 		i++;
 	}
+	free(thread_ids);
+	free(forks);
 	return (0);
 }
